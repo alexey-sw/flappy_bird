3de8a09dbf1fece0f26cd3fee596e076712c941b
Alexey_js <alexey.grishchenko@mail.ru>
2020-10-20 00:45:04 +0300

    Basic classes for the game established, idea commented out

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 # will help us manage our coordinates for bird
+# where will be 3 classes in our game: bird vec and game
+# bird class has pictures of the bird, x,y as properties
+# bird contains methods for its update(x,y update),collision detection
+# game class contains methods for pipe generation(randomised, redrawal of all objects) and game start and game end
+
+# score will be added later
 import load
-arr= load.funct()
+# import keyboard from pynput
+import pygame
+pygame.init()
 class Vec:
     def __init__(self,x,y):
         self.x = x
@@ -10,4 +18,26 @@ class Vec:
         self.y = self.y+addy
 class Bird:
     def __init__(self,images,coord):
+        self.images = images
+        self.coord = coord
+    
+class Game:
+    def __init__(self):
+        pass
+        # self.bg = bg
+        # self.bird = bird
+        # self.state = lost
+        # self.w = w
+        # self.h = h
+    def start(self):
+        global scr
+        scr = pygame.display.set_mode((400,400))
 
+
+game = Game()
+game.start()
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+    if game.state == "lost":
+        continue
