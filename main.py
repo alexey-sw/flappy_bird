2eaf6c9cb715c7a357ea3d31af858bf05012382c
Alexey_js <alexey.grishchenko@mail.ru>
2020-10-20 22:32:00 +0300

    Added y axis acceleration for the bird

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ import pygame,sys
 bdimg = pygame.image.load("images/redbird-midflap.png")
 pygame.init()
 black = 0, 0, 0
-
+clock = pygame.time.Clock()
+# would be great to implement clock.get_fps()
+FPS = 120
 class Vec:
     def __init__(self,x,y):
         self.x = x
@@ -18,16 +20,30 @@ class Vec:
     def add(self,addx,addy):
         self.x = self.x+addx
         self.y = self.y+addy
+    def subtract(self,subtrx,subtry):
+        self.x-=subtrx
+        self.y-=subtry
+    def __repr__(self):
+        return "{} {}".format(self.x,self.y)
 class Bird:
-    stPos = Vec(50,100)
+    whitespace_pressed = False
     def __init__(self,images):
         self.images = images # array
-        self.coord = self.stPos
-    def collide(self,crd=stPos):
+        self.coord = Vec(50,300) # starting position
+        self.speed = Vec(0,0) # starting speed
+    def collide(self): # returns true if bird collides with pipes or ground
         return False
         # collision detection of bird with pipes and ground
     def update(self):
-        scr.blit(self.images,(self.coord.x,self.coord.y))
+        if self.whitespace_pressed:
+            self.speed.add(0,5)
+            self.coord.y-=self.speed.y
+            
+            self.whitespace_pressed=False
+        self.speed.subtract(0,0.05)
+        self.coord.y-=self.speed.y
+        scr.blit(self.images,[self.coord.x,self.coord.y]) # we don't add any speed to x as long as 
+#
     
 class Game:
     def __init__(self,bird,bg):
@@ -52,10 +68,15 @@ game.start()
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key==pygame.K_SPACE:
+                flappy_bird.whitespace_pressed=True
     if game.state == "lost":
         continue
+    
     scr.fill(black)
     game.update()
-    
+    print(clock.get_fps())
+    clock.tick(FPS)
     
 
