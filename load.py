e37b15a7af68db606e783c95dc6066691496a2c0
Alexey_js <alexey.grishchenko@mail.ru>
2020-10-25 11:11:23 +0300

    centered the window, added rects

--- load.py
+++ load.py
@@ -5,54 +5,68 @@ import pygame
 FPS = 60
 black = 0, 0, 0
 clock = pygame.time.Clock()
-globalh = 360*2 
+globalh = 360*2
 globalw = 288*2
-bgh=60
+baseh = 60
+
 
 class Vec:
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-    def add(self,addx,addy):
+
+    def add(self, addx, addy):
         self.x = self.x+addx
         self.y = self.y+addy
-    def subtract(self,subtrx,subtry):
-        self.x-=subtrx
-        self.y-=subtry
+
+    def subtract(self, subtrx, subtry):
+        self.x -= subtrx
+        self.y -= subtry
+
     def __repr__(self):
-        return "{} {}".format(self.x,self.y)
-bgsize = Vec(globalw,int(0.05*globalh))
-def load(): # function responsible for loading and scaling images
+        return "{} {}".format(self.x, self.y)
+
+
+basesize = Vec(globalw, int(0.05*globalh))
+
+
+def load():  # function responsible for loading and scaling images
     global numbers
     numbers = []
-    for i in range(0,10):
+    for i in range(0, 10):
         numbers.append(pygame.image.load("images/{}.png".format(i)))
-    
+
     global birdimgs
     birdimgs = [
         pygame.image.load("images/redbird-upflap.png"),
         pygame.image.load("images/redbird-downflap.png"),
         pygame.image.load("images/redbird-midflap.png")
-        ]
+    ]
 
-    global bgimgs 
-    bgimgs={"bg":pygame.image.load("images/background.jpg"),
-    "gnd":pygame.transform.scale(pygame.image.load("images/base.png"),(bgsize.x,bgsize.y))}
-    
+    global baseimgs
+    baseimgs = {"bgrnd": pygame.image.load("images/background.jpg"),
+                "gnd": pygame.transform.scale(pygame.image.load("images/base.png"), (basesize.x, basesize.y))}
+
     global pipeimgs
-    pipeimgs = {"nrml":pygame.image.load("images/pipe-green.png"),"revr":pygame.image.load("images/pipe-green-reversed.png")}
-    
-    
+    pipeimgs = {"nrml": pygame.image.load(
+        "images/pipe-green.png"), "revr": pygame.image.load("images/pipe-green-reversed.png")}
+
+
 load()
 # global piperectrev
 #     birdrect =  birdimgs[0].get_rect()
 #     piperect = pipeimgs["nrml"].get_rect()
 #     piperectrev = pipeimgs["revr"].get_rect()
-#     gndrect = bgimgs["gnd"].get_rect()
+#     gndrect = baseimgs["gnd"].get_rect()
 
-def getRect(img,obj):
-    return img.get_rect(center=(obj.x,obj.y))
-birdrect = getRect(birdimgs[0],Vec(50,50))
-bgpos = Vec(0,globalh-bgsize.y)
-print(bgpos)
-bgrect = getRect(bgimgs["gnd"],bgpos)
+
+def getRect(img, obj):
+    return img.get_rect(center=(obj.x, obj.y))
+
+
+birdrect = getRect(birdimgs[0], Vec(50+int(birdimgs[0].get_width()/2), 50))
+basepos = Vec(0, globalh-basesize.y)
+baseposrect = Vec(0+int(baseimgs["gnd"].get_width()/2),basepos.y+int(basesize.y/2))
+baserect = getRect(baseimgs["gnd"], baseposrect)
+print(baseposrect)
+print(basepos)
--- main.py
+++ main.py
@@ -13,71 +13,89 @@
 import pygame
 import sys
 from load import *
+import os
+os.environ['SDL_VIDEO_CENTERED'] = '1'
 
-bgimg = pygame.transform.scale(pygame.image.load("images/base.png"),(globalw,bgh))# changing size of our image
+baseimg = pygame.transform.scale(pygame.image.load(
+    "images/base.png"), (globalw, baseh))  # changing size of our image
 #!scale (xsize,ysize)
 pygame.init()
 
 # would be great to implement clock.get_fps()
 
+
 class Bird:
     flapcount = 0
     whitespace_pressed = False
-    def __init__(self,images):
-        self.images = images # array
-        self.coord = Vec(50,50) # starting position
-        self.speed = Vec(0,0) # starting speed
+
+    def __init__(self, images):
+        self.images = images  # array
+        self.coord = Vec(50, 50)  # starting position
+        self.speed = Vec(0, 0)  # starting speed
         self.rect = birdrect
-    def collide(self): # returns true if bird collides with pipes or ground
-        if self.coord.y>=globalh-bgh:
+
+    def collide(self):  # returns true if bird collides with pipes or ground or ceiling
+        if self.rect.colliderect(baserect):
             return True
         return False
         # collision detection of bird with pipes and ground
+
     def update(self):
-        self.flapcount+=0.2
-        if self.flapcount>998:
+        self.flapcount += 0.2
+        if self.flapcount > 998:
             self.flapcount = 0
         if self.whitespace_pressed:
-            self.speed.add(0,9)   
-            self.whitespace_pressed=False
-        self.speed.subtract(0,0.4)
-        self.coord.y-=self.speed.y
-        scr.blit(self.images[int(self.flapcount%3)],[self.coord.x,self.coord.y]) # we don't add any speed to x as long as 
+            self.speed.add(0, 9)
+            self.whitespace_pressed = False
+        self.speed.subtract(0, 0.4)
+        self.coord.y -= self.speed.y
+        self.rect.y=self.coord.y
+        # we don't add any speed to x as long as
+        scr.blit(self.images[int(self.flapcount % 3)],
+                 [self.coord.x, self.coord.y])
+
     def freeze(self):
-        scr.blit(self.images[1],[self.coord.x,self.coord.y])
+        scr.blit(self.images[1], [self.coord.x, self.coord.y])
 #
+
+
 class Game:
-    def __init__(self,bird,bg):
-        self.bg = bg
+    def __init__(self, bird, base):
+        self.base = base
         self.bird = bird
         self.state = "playing"
         self.w = globalw
         self.h = globalh
-         #* size of our background
+        # * size of our background
         self.objects = [self.bird]    # for obj in self objects: i.update
+
     def start(self):
         global scr
-        scr = pygame.display.set_mode((self.w,self.h))
+        scr = pygame.display.set_mode((self.w, self.h))
         pygame.display.set_caption("Flappy bird")
+
     def update(self):
-        scr.blit(self.bg,(bgpos.x,bgpos.y))
+        scr.blit(self.base, (basepos.x, basepos.y))
+        pygame.draw.rect(scr,(255,0,0),baserect,4)
+        pygame.draw.rect(scr,(255,0,0),birdrect,4)
+
         if not self.bird.collide():
             self.bird.update()
         else:
             self.bird.freeze()
         pygame.display.flip()
-        
+
+
 flappy_bird = Bird(birdimgs)
-game = Game(flappy_bird,bgimgs["gnd"])
+game = Game(flappy_bird, baseimgs["gnd"])
 game.start()
 while 1:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key==pygame.K_SPACE:
-                flappy_bird.whitespace_pressed=True
+            if event.key == pygame.K_SPACE:
+                flappy_bird.whitespace_pressed = True
     scr.fill(black)
     game.update()
     clock.tick(FPS)
-    
-
